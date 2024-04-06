37fa532be05ec116998d1186bedb91d8309280de
zulfen <zulfeusmcemail@gmail.com>
2024-04-06 15:11:29 +0100

    Fixed CMakeLists and seperated include dir

--- src/sdl/window/SDLWindow.cpp
+++ src/sdl/window/SDLWindow.cpp
@@ -2,6 +2,7 @@
 #include <SDL.h>
 #include <string>
 
+
 WindowImpl::WindowRef SDLWindow::CreateWindow()
 {
 	SDL_CreateWindow(WindowImpl::title, 0, 0, WindowImpl::width, WindowImpl::height, SDL_WINDOW_OPENGL);
--- src/sdl/window/SDLWindow.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Graphics/window/WindowImpl.h"
-
-class SDLWindow : public WindowImpl {
-	WindowRef CreateWindow();
-};
